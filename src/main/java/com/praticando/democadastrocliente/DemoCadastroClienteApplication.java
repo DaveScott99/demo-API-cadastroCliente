4040cff247a5abc8fd2f5773e5b12c59b3c08ec3
DaviSantos <davicalixto2077@gmail.com>
2022-10-15 16:10:07 -0300

    Projeto criado

---
+++ src/main/java/com/praticando/democadastrocliente/DemoCadastroClienteApplication.java
@@ -0,0 +1,13 @@
+package com.praticando.democadastrocliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCadastroClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCadastroClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/praticando/democadastrocliente/DemoCadastroClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praticando.democadastrocliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCadastroClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
