a356b69fa380bc08f3fd430ae84205a9dfc60bea
DaviSantos <davicalixto2077@gmail.com>
2022-10-15 16:42:00 -0300

    Model e Repository

---
+++ src/main/java/com/praticando/democadastrocliente/repository/ClienteRepository.java
@@ -0,0 +1,82 @@
+package main.java.com.praticando.democadastrocliente.repository;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Optional;
+
+import main.java.com.praticando.democadastrocliente.model.Cliente;
+
+public class ClienteRepository {
+    
+    private List<Cliente> clientes = new ArrayList<>();
+    private Integer ultimoId = 0;
+
+
+    /**
+     * Método para retornar uma lista de clientes.
+     * @return Retorna uma lista de clientes.
+     */
+    public List<Cliente> obterTodos(){
+        return clientes;
+    }
+
+    /**
+     * Método que retorna o cliente pelo seu id. 
+     * @param id do cliente que será localizado.
+     * @return Retorna um cliente caso tenha sido encontrado.
+     */
+    public Optional<Cliente> obterPorId(Integer id){
+        return clientes
+                .stream()
+                .filter(cliente -> cliente.getId == id)
+                .findFirst(); 
+    }
+
+    /**
+     * Método para adicionar clientes na lista.
+     * @param cliente que será adicionado.
+     * @return Retorna o cliente que foi adicionado na lista.
+     */
+    public Cliente adicionar(Cliente cliente){
+        ultimoId++;
+        cliente.setId(ultimoId);
+        clientes.add(cliente);
+
+        return cliente;
+    }
+
+    /**
+     * Método para deletar um cliente por id.
+     * @param id do cliente que será deletado.
+     */
+    public void deletar(Integer id){
+        clientes.removeIf(cliente -> cliente.getId() == id);
+    }
+
+    /**
+     * Método para atualizar o cliente na lista.
+     * @param cliente que será atualizado.
+     * @return Retorna o cliente após atualizar a lista.
+     */
+    public Cliente atualizar(Cliente cliente){
+
+        // Encontrar o cliente na lista
+        Optional<Cliente> clienteEncontrado = obterPorId(cliente.getId());
+
+        if (clienteEncontrado.isEmpty()){
+            throw new InputMismatchException("Cliente não encontrado!");
+        }
+
+        // Remover o cliente antigo da lista
+        deletar(cliente.getId());
+
+        // Adicionar o cliente atualizado na lista.
+        clientes.add(cliente);
+
+        return cliente;
+
+    }
+
+
+}
